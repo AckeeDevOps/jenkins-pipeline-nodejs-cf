7cd19dec2e15fb446cbb4a39d1257f6a40827254
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2019-11-01 09:51:40 +0100

    Fix secret injection in single pipeline mode

--- vars/processNodeCfConfig.groovy
+++ vars/processNodeCfConfig.groovy
@@ -12,6 +12,7 @@ def call(Map cfg, String branch, String build, scm = null){
   config.firebaseSACredentials = cfg.firebaseSACredentials // required
   config.debugMode = cfg.debugMode
   config.runtimeConfig = cfg.runtimeConfig ? cfg.runtimeConfig : [:]
+  config.envDefaults = cfg.envDefaults ? cfg.envDefaults : [:]
 
   // create dummy image tag
   config.dockerImageTag = "build-nodejs-cf-build${config.buildNumber}-${UUID.randomUUID().toString()}"
@@ -23,15 +24,13 @@ def call(Map cfg, String branch, String build, scm = null){
     echo('running in Multibranch pipeline mode')
     //config.repositoryUrl = scm.getUserRemoteConfigs()[0].getUrl()
     config.branch = branch // get branch from the Jenkins runtime details
-    // get default configuration for all branches
-    config.envDefaults = cfg.envDefaults ? cfg.envDefaults : [:]
     // merge defaults with the actual values, actual values have always precedence
     config.envDetails = config.envDefaults + getNodeBranchConfig(cfg, config.branch)
     config.secretsInjection = config.envDetails.secretsInjection
   } else {
     echo('running in Simple pipeline mode')
     // convert top-level configuration to universal envDetails format
-    config.envDetails = [:]
+    config.envDetails = config.envDefaults
     config.envDetails.repositoryUrl = cfg.repositoryUrl
     config.envDetails.gcpProjectId = cfg.gcpProjectId
     config.branch = cfg.branch // get branch from Jenkinsfile
